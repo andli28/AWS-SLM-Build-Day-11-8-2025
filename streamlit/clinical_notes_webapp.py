f36d742c0a4376c0e5fa08637bbcbd29ff720588
Jasmine Lo <37763048+jasminelo2020@users.noreply.github.com>
2025-11-08 13:12:05 -0800

    add clinical notes summarizer web app skeleton

---
+++ streamlit/clinical_notes_webapp.py
@@ -0,0 +1,46 @@
+import streamlit as st
+import numpy as np
+import matplotlib.pyplot as plt
+
+st.write("""
+# Clinical Notes Summarizer - SLM Build Day
+Input a clinical or doctor's note to get a structured JSON summarization!
+""")
+
+clincal_note = st.text_input("Enter your note:")
+
+if clincal_note:
+    st.write(f"Original note:\n {clincal_note}")
+
+    with st.spinner('Analyzing note contents...'):
+
+        # model
+#         genres = model.predict_genre('output.wav', 40)
+
+        # create tabs for each subtopic
+        tab1, tab2, tab3 = st.tabs(["Diagnosis", "Medications", "Structured JSON"])
+        
+        # diagnosis
+        with tab1:
+            st.header("Diagnosis")
+            
+            st.write("""
+            The diagnosis of the note is:
+
+            """)
+        
+        # medication
+        with tab2:
+            st.header("Medications")
+            st.write("""
+            These are the medications recommended for the diagnosis:
+            
+            """)
+                     
+        # structured json 
+        with tab3:
+            st.header("Structured JSON Output")
+            st.write("""
+            Here is the full structured JSON of your note:
+            
+            """)
