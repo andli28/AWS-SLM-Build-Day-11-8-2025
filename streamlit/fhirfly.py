de37d4305a28c22618576f62c5495249a1df340c
Jasmine Lo <37763048+jasminelo2020@users.noreply.github.com>
2025-11-08 14:38:34 -0800

    fix st.logo call

--- streamlit/fhirfly.py
+++ streamlit/fhirfly.py
@@ -3,7 +3,7 @@ import numpy as np
 
 logo_path = "FHIRFly_logo.png" 
 
-st.logo(image=logo_path, size="small", link="https://fhirfly.streamlit.app/") 
+st.logo(logo_path, size="small", link="https://fhirfly.streamlit.app/") 
 
 st.write("""
 # Clinical Notes Summarizer - SLM Build Day
